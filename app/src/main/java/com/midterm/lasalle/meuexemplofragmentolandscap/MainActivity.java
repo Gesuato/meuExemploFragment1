01338c281a51f50bd847c9e4c92c4b03fe88cc02
Unknown <victorgesuato3@gmail.com>
2018-05-18 09:08:13 -0400

    Teste2

---
+++ app/src/main/java/com/midterm/lasalle/meuexemplofragmentolandscap/MainActivity.java
@@ -0,0 +1,13 @@
+package com.midterm.lasalle.meuexemplofragmentolandscap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
